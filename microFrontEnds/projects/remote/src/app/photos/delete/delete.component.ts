eee3472f91632fbe119b3f24f69eea60ee9c6517
AmiMiftahul <ami.miftahul@gmail.com>
2024-07-19 20:05:10 +0700

    delete

--- microFrontEnds/projects/remote/src/app/photos/delete/delete.component.ts
+++ microFrontEnds/projects/remote/src/app/photos/delete/delete.component.ts
@@ -1,10 +1,25 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PhotosService } from '../../services/photos.service';
 
 @Component({
   selector: 'app-delete',
   templateUrl: './delete.component.html',
-  styleUrl: './delete.component.scss'
+  styleUrl: './delete.component.scss',
 })
 export class DeleteComponent {
+  photoId!: Number;
+  constructor(
+    private route: Router,
+    private router: ActivatedRoute,
+    private photosService: PhotosService
+  ) {
+    this.photoId = parseInt(this.router.snapshot.paramMap.get('id') || '');
 
+    // FOR DELETE
+    this.photosService.deletePhoto(this.photoId).subscribe((photo) => {
+      alert('Photo Deleted successfully');
+      this.route.navigate(['photos']);
+    });
+  }
 }
